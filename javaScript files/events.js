017b08dcc8f7ff37ba82ae152ce8eed8c8049cc0
ashishraimse <100270348+ashishraimse@users.noreply.github.com>
2022-12-15 13:06:56 +0530

    1.1.1.5.5

--- javaScript files/events.js
+++ javaScript files/events.js
@@ -1,3 +1,9 @@
 // add new events here--
-var eventName = ["Weekly Contest 2" , "Weekly Contest 3" ,"Weekly Contest 4"];
-document.getElementById("events").innerHTML = eventName.join("\n");
+// var eventName = ["Weekly Contest 2" , "Weekly Contest 3" ,"Weekly Contest 4"];
+// document.getElementById("events").innerHTML = eventName.join("\n");
+function classToggle() 
+{
+    const navs = document.querySelectorAll('.Navbar__Items')
+    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+}
+document.querySelector('.Navbar__Link-toggle').addEventListener('click', classToggle);
