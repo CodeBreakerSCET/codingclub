932926fe640c512048ef4cf85453a4c1e9cdd06f
ashishraimse <100270348+ashishraimse@users.noreply.github.com>
2022-12-30 15:55:54 +0530

    1.1.1.5.8.4

--- javaScript files/events.js
+++ javaScript files/events.js
@@ -2,7 +2,7 @@
 var myfunc = setInterval(function() 
 {
     // code goes here
-    var countDownDate = new Date("Dec 30, 2022 20:00:00").getTime();
+    var countDownDate = new Date("Jan 01, 2023 20:00:00").getTime();
     var now = new Date().getTime();
     var timeleft = countDownDate - now;
         
@@ -29,10 +29,10 @@ var myfunc = setInterval(function()
 
 // add new events here--
 // Don't forget to add time in the events.html for upcoming contest
-const eventName = ["Weekly Contest 3 (Not Yet scheduled) ","Weekly Contest 4 (Not Yet scheduled)  <br>"];
+const eventName = ["Weekly Contest 3 (Not Yet scheduled) <br>","Weekly Contest 4 (Not Yet scheduled)  <br>"];
 function upcomingTest()
 {
-    document.getElementById("first").innerHTML = "Weekly Contest 2 (Scheduled on Next Sunday) <br>";
+    document.getElementById("first").innerHTML = "Weekly Contest 2 (Scheduled on this Sunday) <br>";
     document.getElementById("events").innerHTML = eventName.join("\n");
     document.getElementById("eventBtn").value = "UpComing Contest"
 }
