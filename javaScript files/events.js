d11a48bf8222bd2f9b0457981be212f845a99348
CodeBreakerSCET <codebreaker@sanskar.org>
2023-03-05 16:51:03 +0530

    Contest 3 Update

--- javaScript files/events.js
+++ javaScript files/events.js
@@ -59,3 +59,15 @@ function UpdateContest()
     }
 }
 
+$(document).ready(function() 
+{
+    $("button").click(function() 
+    {
+        var x = $("form").serializeArray();
+        $.each(x, function(i, field) 
+        {
+            // $("#output").append(field.name + ":" + field.value + " ");
+            eventName.push(field.value);
+        });
+    });
+});
